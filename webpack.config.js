905bd331cbb602a9fccd5d3bcdcc7a84933209f8
Filip Jirsák <filip@jirsak.org>
2023-02-16 18:10:37 +0100

    Výchozí commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    filename: 'bundle-[contenthash:6].js',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name]-[contenthash:6].[ext]'
+        },
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '', noErrorOnMissing: true },
+      ],
+    }),
+  ],
+};
